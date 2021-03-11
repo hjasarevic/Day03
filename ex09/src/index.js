12e464c5d1451dff48b1d51c3f08211b94758f63
Haris Jasarevic <haris.jasarevic@gmail.com>
2021-03-11 10:19:47 +0100

    završeno

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,17 +1,6 @@
 function main() {
     // Only change code below this line
-    // Variable declarations
-var studlyCapWar;
-var properCamelCase;
-var titleCaseOver;
-var studlyCapWar = camelCase;
-
-
-
-    // Variable assignments
-var studlyCapWar = 10;
-var properCamelCase = "A String";
-var titleCaseOver = 9000;
+    
 
 
     // Only change code above this line
